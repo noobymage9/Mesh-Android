17de09064adf05956d1469a3025b5974f914f7e0
noobymage9 <46750676+noobymage9@users.noreply.github.com>
2019-10-31 20:06:44 +0900

    Initialisation

--- app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
@@ -4,32 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mesh.R;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    private HomeViewModel homeViewModel;
+public class HomeFragment extends Fragment {
+    private List<ContactInfo> contactList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        RecyclerView recList = root.findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        for (int i = 0; i < 15; i++) {
+            contactList.add(new ContactInfo("Terry", "Seah", "Win"));
+        }
+        ContactAdapter contactAdapter = new ContactAdapter(contactList);
+        recList.setAdapter(contactAdapter);
         return root;
     }
 }
