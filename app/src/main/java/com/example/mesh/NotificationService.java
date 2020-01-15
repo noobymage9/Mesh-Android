4bc098efd31cf0f3066d79ef6ce8cba5aa86f884
noobymage9 <terryseah2009@gmail.com>
2020-01-15 23:10:22 +0800

    Refactored NotificationService

--- app/src/main/java/com/example/mesh/NotificationService.java
+++ app/src/main/java/com/example/mesh/NotificationService.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -15,7 +14,6 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import com.example.mesh.Database.DBManager;
 import com.example.mesh.message.MessageActivity;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
@@ -27,7 +25,7 @@ public class NotificationService extends NotificationListenerService {
     private final String WHATSAPP_PACKAGE = "com.whatsapp";
     private final String TELEGRAM_PACKAGE = "org.telegram.messenger";
     private Context context;
-    private String pack, title, text, sourceApp;
+    private String packageName, title, text, sourceApp;
     private Date currentDate;
     private DBManager dbManager;
 
@@ -41,55 +39,82 @@ public class NotificationService extends NotificationListenerService {
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) { // Reading data from notification
-        pack = sbn.getPackageName();                              // Message source can be obtained from here
-
+        packageName = sbn.getPackageName();                       // Message source can be obtained from here
         Bundle extras = sbn.getNotification().extras;
+
         title = "";
         text = "";
         sourceApp = "";
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-        Date currentDate = null;
+        currentDate = null;
 
         dbManager = new DBManager(this);
         dbManager.open();
 
-        if (pack.equals(WHATSAPP_PACKAGE))
-            sourceApp = "WhatsApp";
-        else if (pack.equals(TELEGRAM_PACKAGE))
-            sourceApp = "Telegram";
+        sourceApp = getSourceApp(packageName);
+        if (sourceApp.length() == 0) return;                   // Source Application is not a Chat Application
 
-        if (extras.containsKey(ANDROID_TITLE_KEY)) {
-            title = extras.getString(ANDROID_TITLE_KEY);
-            // Mostly is contact name. "Whatsapp" and "Telegram" must be thrown
-        }
+        title = getTitle(extras);                             // Retrieve Contact Name
+        if(!isContactName(title)) return;                     // Mostly is contact name. "Whatsapp" and "Telegram" must be thrown
+        if(title.length() == 0) return;                       // No contact name need to throw
 
-        if (title.equals("WhatApp") || title.equals("Line") || title.equals(("Telegram")))
-            return;
+        text = getText(extras);                               // Retrieve Message Content
+        if(text.length() == 0) return;                        // No Message Content need to throw
 
-        if (extras.containsKey(ANDROID_TEXT_KEY)) {               // Retrieve message content
-            if (extras.getCharSequence(ANDROID_TEXT_KEY) != null) {
-                text = extras.getCharSequence(ANDROID_TEXT_KEY).toString();
-            }
-        }
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            currentDate = Calendar.getInstance().getTime();
-        }
+        currentDate = getCurrentDate();
+        if(currentDate == null) return;                       // If android version do not support Calendar
 
         dbManager.insertMessage(title, text, sourceApp, currentDate);
         dbManager.insertContact(title);
         dbManager.close();
-        // Sending results to message activity
 
-        Intent i = new Intent(MessageActivity.RECEIVE_JSON);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(i);
-
+        // Notify HomeFragment and MessageActivity upon receiving new messages
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MessageActivity.RECEIVE_JSON));
     }
+
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
 
     }
 
+    private String getSourceApp(String pack) {
+        switch(pack) {
+            case WHATSAPP_PACKAGE: return "WhatsApp";
+            case TELEGRAM_PACKAGE: return "Telegram";
+            default: return "";
+        }
+    }
+
+    private String getTitle(Bundle extras) {
+        if (extras.containsKey(ANDROID_TITLE_KEY)) {
+            return extras.getString(ANDROID_TITLE_KEY);
+        }
+        return "";
+    }
+
+    private String getText(Bundle extras) {
+        if (extras.containsKey(ANDROID_TEXT_KEY)) {
+            CharSequence charSequence = extras.getCharSequence(ANDROID_TEXT_KEY);
+            if (charSequence != null) {
+                return charSequence.toString();
+            }
+        }
+        return "";
+    }
+
+    private Date getCurrentDate() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            return Calendar.getInstance().getTime();
+        }
+        return null;
+    }
+
+    private boolean isContactName(String title) {
+        if (title.equals("Whatsapp") ||
+            title.equals("Line") ||
+            title.equals("Telegram"))
+            return false;
+        return true;
+    }
 
 }
