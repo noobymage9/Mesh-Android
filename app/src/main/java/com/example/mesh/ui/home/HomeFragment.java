49aaef6f56cf44b1513664dc8e2baba049e1406d
noobymage9 <terryseah2009@gmail.com>
2020-01-16 09:30:02 +0800

    Semi-Refactored HomeFragment

--- app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
@@ -4,11 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,38 +12,41 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mesh.Database.DBManager;
-import com.example.mesh.Database.DatabaseHelper;
 import com.example.mesh.R;
 import com.example.mesh.message.MessageActivity;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
     private ArrayList<ContactInfo> contactList;
+    private ArrayList<String> contactNames;
+
+    private View root;
     private DBManager dbManager;
+    private RecyclerView recyclerView;
     private ContactAdapter contactAdapter;
+    private LocalBroadcastManager localBroadcastManager;
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            contactAdapter.updateByName(getContactNames());
+        }
+    };
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-        // Recycler View
-        RecyclerView recList = root.findViewById(R.id.contactList);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(getContext());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
-
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        contactNames = getContactNames();
+        initialiseRecyclerView(root);
+        initialiseLocalBroadcastManager();
 
         //contactList = getContacts();
-        ArrayList<String> contactNames = getContactNames();
+
 
         /*
         for (int i = 0; i < 15; i++) { // Generating sample contacts
@@ -59,20 +58,13 @@ public class HomeFragment extends Fragment {
         }
         */
 
-        LocalBroadcastManager bManager = LocalBroadcastManager.getInstance(getContext());
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(MessageActivity.RECEIVE_JSON);
-        bManager.registerReceiver(bReceiver, intentFilter);
-
-        contactAdapter = new ContactAdapter(contactNames);
-        recList.setAdapter(contactAdapter);
         return root;
     }
 
     private ArrayList<ContactInfo> getContacts(){
         ArrayList<String> allNames = getContactNames();
         ArrayList<Integer> allIDs = getContactIDs();
-        ArrayList<ContactInfo> allContacts = new ArrayList<ContactInfo>();
+        ArrayList<ContactInfo> allContacts = new ArrayList<>();
         for (int i = 0; i < allNames.size(); i++) {
             allContacts.add(new ContactInfo(allIDs.get(i), allNames.get(i)));
         }
@@ -80,27 +72,34 @@ public class HomeFragment extends Fragment {
     }
 
     private ArrayList<Integer> getContactIDs() {
-        ArrayList<Integer> temp = null;
+        ArrayList<Integer> temp;
         dbManager = new DBManager(getContext());
         dbManager.open();
         temp = dbManager.fetchAllContactIDs();
+        dbManager.close();
         return temp;
     }
 
 
     private ArrayList<String> getContactNames() {
-        ArrayList<String> temp = null;
+        ArrayList<String> temp;
         dbManager = new DBManager(getContext());
         dbManager.open();
         temp = dbManager.fetchAllContactNames();
+        dbManager.close();
         return temp;
     }
 
-    private BroadcastReceiver bReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            contactAdapter.updateByName(getContactNames());
-        }
-    };
+    private void initialiseRecyclerView(View root) {
+        recyclerView = root.findViewById(R.id.contactList);
+        recyclerView.setHasFixedSize(true);
+        contactAdapter = new ContactAdapter(contactNames);
+        recyclerView.setAdapter(contactAdapter);
+    }
+
+    private void initialiseLocalBroadcastManager() {
+        localBroadcastManager = LocalBroadcastManager.getInstance(getContext());
+        localBroadcastManager.registerReceiver(broadcastReceiver, new IntentFilter(MessageActivity.RECEIVE_JSON));
+    }
 
 }
