cf652b444b780969e5ab514c665f6374b4b36512
noobymage9 <terryseah2009@gmail.com>
2020-01-17 15:09:35 +0800

    Refactored the imports

--- app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mesh/ui/home/HomeFragment.java
@@ -1,9 +1,6 @@
 package com.example.mesh.ui.home;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +8,17 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mesh.Database.DBManager;
 import com.example.mesh.R;
-import com.example.mesh.message.MessageActivity;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-    private ArrayList<ContactInfo> contactList;
-
     private View root;
-    private DBManager dbManager;
     private RecyclerView recyclerView;
     private ContactAdapter contactAdapter;
-    private LocalBroadcastManager localBroadcastManager;
     private HomeViewModel homeViewModel;
 
 
