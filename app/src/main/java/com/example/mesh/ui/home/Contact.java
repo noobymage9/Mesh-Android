0755c09d2535c0911612f852d0be8584202f68aa
noobymage9 <terryseah2009@gmail.com>
2020-01-17 15:38:32 +0800

    Refactored

---
+++ app/src/main/java/com/example/mesh/ui/home/Contact.java
@@ -0,0 +1,31 @@
+package com.example.mesh.ui.home;
+
+import java.sql.Blob;
+
+public class Contact {
+    protected int id;
+    protected Blob icon;
+    protected String name;
+
+    public Contact(int id, Blob icon, String name) {
+        this.id = id;
+        this.icon = icon;
+        this.name = name;
+    }
+
+    public Contact(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getID() {return this.id;}
+
+    public Blob getBlob() {
+        return this.icon;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
