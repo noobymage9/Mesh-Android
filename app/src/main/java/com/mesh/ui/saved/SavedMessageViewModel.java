66c0d097d63b58247a4b01228b17623c9abe3004
Terry Seah <terryseah2009@gmail.com>
2020-02-16 17:27:42 +0800

    Quick Fix for adapter refresh

--- app/src/main/java/com/mesh/ui/saved/SavedMessageViewModel.java
+++ app/src/main/java/com/mesh/ui/saved/SavedMessageViewModel.java
@@ -1,12 +1,17 @@
 package com.mesh.ui.saved;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.mesh.Database.DBManager;
 import com.mesh.message.Message;
@@ -18,9 +23,17 @@ import java.util.ArrayList;
 public class SavedMessageViewModel extends AndroidViewModel {
     private MutableLiveData<ArrayList<Message>> messages;
     private UserCollection userCollection;
+    private LocalBroadcastManager localBroadcastManager;
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            loadMessages();
+        }
+    };
 
     public SavedMessageViewModel(@NonNull Application application) {
         super(application);
+        initialiseLocalBroadcastManager();
     }
 
     public LiveData<ArrayList<Message>> getMessages(UserCollection userCollection) {
@@ -39,4 +52,8 @@ public class SavedMessageViewModel extends AndroidViewModel {
         }).start();
     }
 
+    private void initialiseLocalBroadcastManager() {
+        localBroadcastManager = LocalBroadcastManager.getInstance(this.getApplication());
+        localBroadcastManager.registerReceiver(broadcastReceiver, new IntentFilter(com.mesh.MainActivity.RECEIVE_JSON));
+    }
 }
