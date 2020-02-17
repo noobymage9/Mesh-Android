831d460088059f7a2c3847a4e8472368e7afa3e7
noobymage9 <terryseah2009@gmail.com>
2020-02-17 16:40:25 +0800

    Added listener to enter button on keyboard for adding tags

--- app/src/main/java/com/mesh/message/SaveDialog.java
+++ app/src/main/java/com/mesh/message/SaveDialog.java
@@ -4,11 +4,16 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +22,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mesh.Database.DBManager;
 import com.mesh.MainActivity;
 import com.mesh.R;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -38,7 +45,7 @@ public class SaveDialog extends Dialog {
         initialiseEditText();
         initialiseAddButton();
         initialiseSaveButton();
-        initialiseRecyclerView();
+        initialiseRecyclerView("");
 
     }
 
@@ -72,6 +79,32 @@ public class SaveDialog extends Dialog {
 
     private void initialiseEditText() {
         editText = findViewById(R.id.add_field);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    add.performClick();
+                }
+                return false;
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO: 17/2/2020 uncomment when DB is ready
+                //initialiseRecyclerView(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void initialiseAddButton() {
@@ -87,16 +120,19 @@ public class SaveDialog extends Dialog {
             dbManager.close();
             if (added) {
                 editText.setText("");
-                initialiseRecyclerView();
+                initialiseRecyclerView("");
                 recyclerView.scrollToPosition(userCollections.size() - 1);
             }
         });
     }
 
-    private void initialiseRecyclerView(){
+    private void initialiseRecyclerView(String filter){
         DBManager dbManager = new DBManager(getContext());
         dbManager.open();
-        userCollections = dbManager.getAllUserCollections();
+        if (filter.equals(""))
+            userCollections = dbManager.getAllUserCollections();
+        else
+            // TODO: 17/2/2020 get tags based on filter
         dbManager.close();
         recyclerView = findViewById(R.id.tag_list);
         recyclerView.setHasFixedSize(true);
